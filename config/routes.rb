f32f924cb3766e472e44be4f74148e62b77e4dba
Pablo Fanjul <pfanjul81@gmail.com>
2024-02-01 10:42:34 +0100

    Rails wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   # # Delete
   # delete "articles/:id", to: "articles#destroy"
   resources :articles
+  root to: 'articles#index'
 end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
